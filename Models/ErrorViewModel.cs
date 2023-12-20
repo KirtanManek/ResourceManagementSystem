c85426004c5c2bd73ada7c2aab989be3dc4d7efa
KirtanManek <manekkirtan@gmail.com>
2023-12-20 11:05:50 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ResourceManagementSystem.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
